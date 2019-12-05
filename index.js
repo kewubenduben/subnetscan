d2af8be1c39a2fc17782fb83db813aa236a178c9
4stclazn <54495947+4stclazn@users.noreply.github.com>
2019-12-05 15:46:24 +1100

    icmp echo request

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const { spawn } = require('child_process');
+
 let ipOctets = '100.87.0.1'.split('.');
 const ports = [22, 23, 80, 443];
 
-let i = 3;
-while (i in ipOctets) {
-  console.log(ipOctets);
+const ping = spawn('ping', ['-c1', '-w1', ipOctets.join('.')])
+  .on('exit', (code) => {
+    console.log(code);
+  });
 
-  ipOctets[i]++;
-  if (255 < ip[i]) {
-    ipOctets[i] = 0;
-    i--;
-    continue;
-  }
-  i = 3;
-}
+// let i = 3; // todo ipv4 enumeration doesnt terminate elegantly (i.e., scans 255.255.255.0, 255.255.0.0, ...)
+// while (i in ipOctets) {
+//   console.log(ipOctets);
+//
+//   ipOctets[i]++;
+//   if (255 < ipOctets[i]) {
+//     ipOctets[i] = 0;
+//     i--;
+//     continue;
+//   }
+//   i = 3;
+// }
