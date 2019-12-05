e89e69e45e1416c027cf1e1e4e75b3c2af9f4dfb
4stclazn <54495947+4stclazn@users.noreply.github.com>
2019-12-05 15:52:45 +1100

    ping enumerated ipv4

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const { spawn } = require('child_process');
-
+// todo cli argument handler
 let ipOctets = '100.87.0.1'.split('.');
 const ports = [22, 23, 80, 443];
 
-const ping = spawn('ping', ['-c1', '-w1', ipOctets.join('.')])
-  .on('exit', (code) => {
-    console.log(code);
+let i = 3; // todo elegantly terminate enumeration; dont scan 255.255.255.0, 255.255.0.0, ...
+while (i in ipOctets) {
+  var ping = spawn('ping', ['-c1', '-w1', ipOctets.join('.')], { // flags platform dependent
+    stdio: 'inherit'
   });
 
-// let i = 3; // todo ipv4 enumeration doesnt terminate elegantly (i.e., scans 255.255.255.0, 255.255.0.0, ...)
-// while (i in ipOctets) {
-//   console.log(ipOctets);
-//
-//   ipOctets[i]++;
-//   if (255 < ipOctets[i]) {
-//     ipOctets[i] = 0;
-//     i--;
-//     continue;
-//   }
-//   i = 3;
-// }
+  ipOctets[i]++;
+  if (255 < ipOctets[i]) {
+    ipOctets[i] = 0;
+    i--;
+    continue;
+  }
+  i = 3;
+}
