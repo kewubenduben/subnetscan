621d0a7c6272ce3a13b018d07369aa54c8a16915
4stclazn <54495947+4stclazn@users.noreply.github.com>
2019-12-07 00:27:16 +1100

    update index.js

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
-// todo: cli argument handler
-let ip = '100.87.0.1';
-const ports = [22, 23, 80, 443];
 
 async function ping(ip) {
-  await exec(`ping -c1 -w1 ${ip}`) // flags os dependent
-    .then(stdout => console.log(stdout)) // todo: only pipe stdout to tty if ping closes with exit_code 0
-    .catch(stderr => console.error(stderr))
+  let { stdout } = await exec(`ping -c1 -w1 ${ip}`) // ping arg flags os dependent
+    .catch(() => {});
+  return stdout;
 };
 
 function increment(ip) {
-  let ipOctets = ip.split('.');
-  for (let i = ipOctets.length - 1; 0 < i; i--)
-    if (ipOctets[i] <= 255) {
-      ipOctets[i]++;
-      break;
-    } else
-      ipOctets[i] = 0;
-
-  return ipOctets.join('.');
-}
-
-const enumeratePing = async _ => { // todo: use an async-await friendly loop
-  while (ip !== '255.255.255.255') {
-    await ping(ip);
-    // todo: spawn worker threads with require('cluster') for every cpu core to scan tcp/udp ports of reachable host with require('net').Socket
-    ip = increment(ip);
+  let ipOctets = ip.split('.')
+  for (let i = ipOctets.length - 1; i in ipOctets; i--) {
+    ipOctets[i]++
+    if (ipOctets[i] > 255)
+      ipOctets[i] = 0
+    else
+      break
   }
+  return ipOctets.join('.')
 }
-enumeratePing();
+
+// todo: cli argument handler
+const ipStart = '100.87.0.1';
+const ipEnd = '100.87.0.3';
+const ports = [22, 23, 80, 443];
+
+let ip = ipStart;
+let promises = [];
+while(ip !== ipEnd) { // todo: fix prone to overflow if ipEnd supplied with invalid ip address
+  promises.push(ping(ip));
+  ip = increment(ip);
+};
+
+Promise.allSettled(promises)
+  .then((results) => {
+    results.forEach((result) => {
+      console.log(result)
+      // todo: spawn worker threads for every cpu core to scan tcp/udp ports of reachable host
+    })
+  });
