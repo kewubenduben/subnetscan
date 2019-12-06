4d74a21e315949bd326c51fd1aa3c8f3e7ab19b3
4stclazn <54495947+4stclazn@users.noreply.github.com>
2019-12-06 14:48:46 +1100

    promisify subprocess

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const { spawn } = require('child_process');
-// todo cli argument handler
-let ipOctets = '100.87.0.1'.split('.');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+// todo: cli argument handler
+let ip = '100.87.0.1';
 const ports = [22, 23, 80, 443];
 
-let i = 3; // todo elegantly terminate enumeration; dont scan 255.255.255.0, 255.255.0.0, ...
-while (i in ipOctets) {
-  var ping = spawn('ping', ['-c1', '-w1', ipOctets.join('.')], { // flags platform dependent
-    stdio: 'inherit'
-  });
+async function ping(ip) {
+  await exec(`ping -c1 -w1 ${ip}`) // flags os dependent
+    .then(stdout => console.log(stdout)) // todo: only pipe stdout to tty if ping closes with exit_code 0
+    .catch(stderr => console.error(stderr))
+};
 
-  ipOctets[i]++;
-  if (255 < ipOctets[i]) {
-    ipOctets[i] = 0;
-    i--;
-    continue;
-  }
-  i = 3;
+function increment(ip) {
+  let ipOctets = ip.split('.');
+  for (let i = ipOctets.length - 1; 0 < i; i--)
+    if (ipOctets[i] <= 255) {
+      ipOctets[i]++;
+      break;
+    } else
+      ipOctets[i] = 0;
+
+  return ipOctets.join('.');
 }
+
+const enumeratePing = async _ => { // todo: use an async-await friendly loop
+  while (ip !== '255.255.255.255') {
+    await ping(ip);
+    // todo: spawn worker threads with require('cluster') for every cpu core to scan tcp/udp ports of reachable host with require('net').Socket
+    ip = increment(ip);
+  }
+}
+enumeratePing();
