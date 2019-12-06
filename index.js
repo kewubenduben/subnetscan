17172b3546a4ab0f6a9734887531368f56efe4ea
4stclazn <54495947+4stclazn@users.noreply.github.com>
2019-12-07 01:06:10 +1100

    stdout rejected promises

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 async function ping(ip) {
-  let { stdout } = await exec(`ping -c1 -w1 ${ip}`) // ping arg flags os dependent
-    .catch(() => {});
-  return stdout;
+  let { error, stdout, stderr } = await exec(`ping -c1 -w1 ${ip}`); // ping arg flags os dependent
+  return stdout || stderr;
 };
 
 function increment(ip) {
-  let ipOctets = ip.split('.')
+  let ipOctets = ip.split('.');
   for (let i = ipOctets.length - 1; i in ipOctets; i--) {
-    ipOctets[i]++
+    ipOctets[i]++;
     if (ipOctets[i] > 255)
-      ipOctets[i] = 0
+      ipOctets[i] = 0;
     else
-      break
+      break;
   }
-  return ipOctets.join('.')
+  return ipOctets.join('.');
 }
 
 // todo: cli argument handler
-const ipStart = '100.87.0.1';
-const ipEnd = '100.87.0.3';
-const ports = [22, 23, 80, 443];
+const ipStart = '100.87.0.1',
+  ipEnd = '100.87.0.3',
+  ports = [22, 23, 80, 443];
 (function(ipStart, ipEnd) {
   let promises = [];
   for (let ip = ipStart; ip !== ipEnd; ip = increment(ip))
@@ -31,7 +30,7 @@ const ports = [22, 23, 80, 443];
   Promise.allSettled(promises)
     .then((results) => {
       results.forEach((result) => {
-        console.log(result); // todo: only stdout ping results with exit_code 0, i.e. resolved promises
+        console.log(result); // todo: filter ping stdout with non-0 exit_code, i.e. rejected promises
         // todo: spawn worker threads for every cpu core to scan tcp/udp ports of reachable host
       });
     });
