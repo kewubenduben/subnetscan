549056a3f9af22e191e8c41b00bc466f68804789
4stclazn <54495947+4stclazn@users.noreply.github.com>
2019-12-07 14:46:28 +1100

    update index.js

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ const { promisify } = require('util'),
   exec = promisify(require('child_process').exec),
   cluster = require('cluster');
 
-// todo: cli args handler
-const ports = [22, 23, 80, 443],
-  pingArgs = ['ping', '-c1', '-w1'],
-  ipRange = {
-    ipStart: '100.87.0.1',
-    ipEnd: '100.87.1.0'
-  };
-let promises = [];
-
+// todo?: up the timeout for high latency but reachable hosts
 // ping arg flags os dependent
-// up the timeout for high latency but reachable hosts
-async function ping(pingArgs, ip) {
+async function ping(argArray, ip) {
   let { error, stdout, stderr } = await exec(`${pingArgs.join(' ')} ${ip}`);
   return (stdout ? [ip, stdout] : [error, stderr]);
 };
@@ -31,13 +22,13 @@ function increment(ip) {
   return ipOctets.join('.');
 }
 
-const pingEnumerated = ((pingArgs, ipStart, ipEnd, promiseArray) => {
-  // todo: fix prone to overflow if ipEnd invalid
+function pingEnumerated((pingArgs, ipStart, ipEnd, pingPromises) => {
+  // todo!: fix prone to overflow if ipEnd invalid
   // todo: ipEnd be inclusive
-  // scan in series not parallel to avoid "fork bomb" slowdowns?
+  // todo?: scan in series not parallel to avoid "fork bomb" slowdowns
   for (let ip = ipStart; ip !== ipEnd; ip = increment(ip)) {
     process.stdout.write(ip);
-    promiseArray.push(ping(pingArgs, ip));
+    pingPromises.push(ping(pingArgs, ip));
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
   };
@@ -45,16 +36,26 @@ const pingEnumerated = ((pingArgs, ipStart, ipEnd, promiseArray) => {
 
   return promiseArray;
 });
-promises = pingEnumerated(pingArgs, ipRange['ipStart'], ipRange['ipEnd'], promises);
 
+// todo!: spawn worker threads for every cpu core to scan tcp/udp ports of reachable hosts
+// todo: less implicit conditional for resolved only
+// todo: cli args handler
+// todo?: port scan traceroute hops
+// console.log(results) omits due to limited buffer size(?)
+const ports = [22, 23, 80, 443],
+  ipRange = {
+    ipStart: '100.87.0.1',
+    ipEnd: '100.87.1.0'
+  },
+  pingArgs = ['ping', '-c1', '-w1'];
+let pingPromises = [];
+promises = pingEnumerated(pingArgs, ipRange['ipStart'], ipRange['ipEnd'], promises);
 (function(promiseArray) {
   Promise.allSettled(promiseArray)
     .then((promiseArray) => {
-      promiseArray.forEach((promise) => { // console.log(results) omits due to limited buffer size(?)
-        if (value = promise['value']) { // todo: less implicit conditional for resolved only
+      promiseArray.forEach((promise) => {
+        if (value = promise['value']) {
           console.log(value);
-          // todo: spawn worker threads for every cpu core to scan tcp/udp ports of reachable hosts
-          // todo: traceroute port scan hops
         }
       });
     });
